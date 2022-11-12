f2291b48c2015ce7105270dc8b6fd93b947249a4
Tam Do <103426061+fuota@users.noreply.github.com>
2022-11-12 15:39:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+#Retrieve last high score
+with open("data.txt", "r") as file:
+    high_score = int(file.read())
+
+scoreboard.high_score = high_score
+scoreboard.update_scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+        with open("data.txt", "w") as file:
+            file.truncate()
+            file.write(f"{scoreboard.high_score}")
+
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+            with open("data.txt", "w") as file:
+                file.truncate()
+                file.write(f"{scoreboard.high_score}")
+
+screen.exitonclick()
